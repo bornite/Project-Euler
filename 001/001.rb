47b2f620544224efe24f6e94f41f3a648f1ab591
bornite <tmkoikee@gmail.com>
2020-12-28 23:50:14 +0900

    :tada: Use endless method definition.

--- 001/001.rb
+++ 001/001.rb
@@ -1,6 +1,4 @@
-def sum_of_multiple_of_3_and_5(limit)
-  return [*1..(limit-1)].select{ |n| n%3 * n%5 == 0 }.sum
-end
+def sum_of_multiple_of_3_and_5(limit) = [*1..(limit-1)].select{ |n| n%3 * n%5 == 0 }.sum
 
 limit = 1000
 p sum_of_multiple_of_3_and_5(limit)
