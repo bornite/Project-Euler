a5bfb5d75864be40da663e871412fe166673b29d
bornite <tmkoikee@gmail.com>
2021-01-01 21:34:24 +0900

    :sparkles:  Add a Ruby code to solve problem 009.

---
+++ 009/009.rb
@@ -0,0 +1,14 @@
+def seach_special_pythagorean_triplet(n)
+   [*1..(n/3)+1].each do |a|
+     [*(a+1)..(n-2)].each do |b|
+       next if a*a+b*b > 998*998
+       c = 1000-a-b
+       if a*a+b*b == c*c
+         return [a, b, c]
+       end
+     end
+   end
+end
+
+n = 1000
+p seach_special_pythagorean_triplet(n).inject(1) {|result, i| result * i }
