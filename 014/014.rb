8460ac4c943daa3df5cf62666f98f4cd5979a507
bornite <tmkoikee@gmail.com>
2021-01-04 18:07:18 +0900

    :tada: Add code to solve problem 014.

---
+++ 014/014.rb
@@ -0,0 +1,17 @@
+def get_length_collaz_sequence(n, count)
+   if n == 1
+      return count+1
+   elsif n.even?
+      return get_length_collaz_sequence(n.to_i/2, count+1)
+   else
+      return get_length_collaz_sequence(3*n.to_i+1, count+1)
+   end
+end
+  
+length = [0, 1]
+2.step(1000000, 1) do |n|
+   length[n] = collaz_sequence(n, 0)
+end
+
+p length.max
+p length.index(length.max).to_s
---
+++ 014/014.rs
@@ -0,0 +1,31 @@
+fn get_length_of_terms_collatz_sequence(n: u64) -> u64 {
+    let mut i: u64 = n;
+    let mut length: u64 = 1;
+    loop {
+        if i == 1 {
+            break;
+        }
+        if i % 2 == 0 {
+            i = i/2;
+        }
+        else {
+            i = 3*i + 1;
+        }
+        length = length + 1;
+    }
+    return length;
+}
+
+fn main() {
+    let mut n: u64;
+    let mut max_len_of_seqs: (u64, u64); /* (number, Length of the Collatz sequence starting from `number`) */
+    max_len_of_seqs = (1, 1);
+    for n in 2..1000000+1 {
+        let length: u64 = get_length_of_terms_collatz_sequence(n);
+        println!("number:{}, terms:{}", n, length);
+        if max_len_of_seqs.1 < length {
+            max_len_of_seqs = (n, length);
+        }
+    }
+    println!("number:{}, terms:{}", max_len_of_seqs.0, max_len_of_seqs.1);
+}
