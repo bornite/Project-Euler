db5b806187300f664799a3956dbec5b85df4285a
bornite <tmkoikee@gmail.com>
2021-01-07 21:22:53 +0900

    :sparkles: Add a Ruby code to solve problem 017.

---
+++ 017/017.rb
@@ -0,0 +1,65 @@
+# until 20
+numbers = [
+  "",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+  "twenty"
+]
+
+# thirty to ninety step by 10
+numbers[30] = "thirty"
+numbers[40] = "forty"
+numbers[50] = "fifty"
+numbers[60] = "sixty"
+numbers[70] = "seventy"
+numbers[80] = "eighty"
+numbers[90] = "ninety"
+
+result = 0
+999.times do |n|
+  hundreds = 100*(n+1).div(100)
+  tens = 10*(n+1).modulo(100).div(10)
+  ones = (n+1).modulo(10)
+
+  # 100の位
+  if hundreds > 0
+    # puts numbers[hundreds.div(100)]
+    result += numbers[hundreds.div(100)].length
+    result += "hundred".length
+
+    # If not divisible by 100, "and" is required
+    if (tens+ones) != 0
+      result += "and".length
+    end
+  end
+
+  # ones place, tens place
+  if (tens+ones).between?(1,20)
+    result += numbers[tens+ones].length
+  elsif (tens+ones) <= 99
+    result += numbers[tens].length
+    result += numbers[ones].length
+  end
+end
+
+# 1000
+result += "one thousand".delete(" ").length
+
+p result
