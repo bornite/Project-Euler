6741f8ffd8239f652cf4b392d9d58bc600c12e79
bornite <tmkoikee@gmail.com>
2021-01-09 17:19:59 +0900

    :recycle: Refactoring.

--- 019/019.rb
+++ 019/019.rb
@@ -12,19 +12,24 @@ number_of_days_in_month = {
     "Nov": 30,
     "Dec": 31,
 }
-days = 1 # 1901-01-01 is Tuesday
-count_of_sunday = 0
 
-p number_of_days_in_month
-1901.upto(2000) do |y|
-   number_of_days_in_month.each do |month, num_of_days|
-      days = days + num_of_days
-      if (y+1).div(4) == 0 && month == :Feb
-         days = days + 1
-      end
-      if days % 7 == 6
-        count_of_sunday = count_of_sunday + 1
+def count_months_in_which_first_day_is_sunday(number_of_days_in_month)
+   count = 0
+   days = 1 # 1901-01-01 is Tuesday
+   p number_of_days_in_month
+
+   1901.upto(2000) do |y|
+      number_of_days_in_month.each do |month, num_of_days|
+         days = days + num_of_days
+         if (y+1).div(4) == 0 && month == :Feb
+            days = days + 1
+         end
+         if days % 7 == 6
+            count = count + 1
+         end
       end
    end
+   return count
 end
-p count_of_sunday
+
+p count_months_in_which_first_day_is_sunday(number_of_days_in_month)
