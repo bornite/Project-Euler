aad78ae0029c7a1be30281e640e0c0a96714ee67
bornite <tmkoikee@gmail.com>
2021-01-16 16:46:27 +0900

    :tada: Add a method to Rational to convert to a recurring decimal.

--- 026/026.rb
+++ 026/026.rb
@@ -1,3 +1,33 @@
-p (2..10).map { |d| 
-  (1..d).detect(lambda{0}) { |t| (10**t % d) == 1 } 
+class Rational
+  def to_rec_decimal
+    f, num = (self < 0) ? ["-", -self] : ["", self]
+
+    i = num.to_i
+    result = f + i.to_s
+    ra = num - i
+    return result if ra.zero?
+    result += "."
+
+    remainder = ra.numerator
+    deno      = ra.denominator
+    place = []
+    rems  = []
+    begin
+      rems  << remainder
+      place << remainder * 10 / deno
+      remainder = remainder * 10 % deno
+      return result + place.join if remainder.zero?
+    end while not (idx = rems.find_index(remainder))
+    place.insert(idx, "(")
+    result + place.join + ")"
+  end
+end
+
+max = 0
+array = (1..1000).map { |d|
+r = Rational(1, d).to_rec_decimal()
+  i = r.index("(")
+  j = r.index(")")
+  cycle_length = (i.nil? || j.nil?) ? 0 : (j - i - 1)
 }
+p array.index(array.max)
