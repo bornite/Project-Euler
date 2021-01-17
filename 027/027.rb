cd3f20858d62a89f8f38b53915de8acf85880f61
bornite <tmkoikee@gmail.com>
2021-01-17 12:51:47 +0900

    :recycle: Refactoring.

--- 027/027.rb
+++ 027/027.rb
@@ -1,11 +1,11 @@
 require 'prime'
 
-def polynomial(a, b, n)
-	return (n.to_i)**2 + (a.to_i)*(n.to_i) + b.to_i
-end
+def polynomial(a, b, n) = (n.to_i)**2 + (a.to_i)*(n.to_i) + b.to_i
 
 a = (-999..999)
 b = (-999..999)
-p a.to_a.product(b.to_a).map {|a, b|
+result = a.to_a.product(b.to_a).map {|a, b|
 	[(0..100).take_while { |n| (n**2 + a*n + b).prime? }.count, a , b]
 }.max
+p result
+p result[1].to_i * result[2].to_i
