2e34fc62dc5ed4884cd25c145ae0588ff446faf7
bornite <tmkoikee@gmail.com>
2021-01-18 19:57:34 +0900

    :recycle: Refactoring.

--- 027/027.rb
+++ 027/027.rb
@@ -5,7 +5,7 @@ def polynomial(a, b, n) = (n.to_i)**2 + (a.to_i)*(n.to_i) + b.to_i
 a = (-999..999)
 b = (-999..999)
 result = a.to_a.product(b.to_a).map {|a, b|
-	[(0..100).take_while { |n| (n**2 + a*n + b).prime? }.count, a , b]
+	[(0..100).take_while { |n| polynomial(a, b, n).prime? }.count, a , b]
 }.max
 p result
 p result[1].to_i * result[2].to_i
