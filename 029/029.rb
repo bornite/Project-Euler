7debffd7d8f37d2b15f3344087290eb1d4767646
bornite <tmkoikee@gmail.com>
2021-01-21 13:00:14 +0900

    :recycle: Refactoring.

--- 029/029.rb
+++ 029/029.rb
@@ -1,1 +1,6 @@
-p (2..100).to_a.product((2..100).to_a).map { |a,n| a**n }.uniq.count
+def get_seq_of_distinct_terms(a, b)
+	a.to_a.product(b.to_a).map { |i,j| i**j }
+end
+
+p get_seq_of_distinct_terms((2..5),(2..5)).uniq.count
+p get_seq_of_distinct_terms((2..100),(2..100)).uniq.count
