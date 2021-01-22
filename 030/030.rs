2a6b2dfb7b60129530c7d86d04834678d2ba5755
bornite <tmkoikee@gmail.com>
2021-01-22 23:39:48 +0900

    :wastebasket: Remove unuse variable.

--- 030/030.rs
+++ 030/030.rs
@@ -4,7 +4,6 @@ fn sum_of_digit_fifth_powers(n: u64) -> u64 {
 }
 
 fn main() {
-    let mut sum: u64 =  0;
     let range = 2..999999;
     let result = range.filter(|i| *i == sum_of_digit_fifth_powers(*i)).fold(0, |sum, x| sum + x);
     println!("{}", result);
