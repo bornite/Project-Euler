13623fd66d6c5b50b5b9fb5b5ce1afec942c26ab
bornite <tmkoikee@gmail.com>
2021-01-24 14:46:24 +0900

    :recycle: Refactoring.

--- 032/032.rs
+++ 032/032.rs
@@ -1,13 +1,9 @@
 use std::collections::HashSet;
 
 fn is_pandigital(a: i32, b: i32) -> bool {
-    let c: i32 = a * b;
-    let a_string = a.to_string();
-    let b_string = b.to_string();
-    let c_string = c.to_string();
-    let check_string = a_string + &b_string + &c_string;
-    let mut check_string_digit = check_string.chars().collect::<Vec<char>>();
+    let check_string = a.to_string() + &b.to_string() + &(a*b).to_string();
     let pandigital_digit = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let mut check_string_digit = check_string.chars().collect::<Vec<char>>();
 
     check_string_digit.sort();
     if check_string_digit == pandigital_digit {
