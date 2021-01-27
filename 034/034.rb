8d9b5c536ed0c1a364baa5827a84f632a45abef7
bornite <tmkoikee@gmail.com>
2021-01-27 23:38:27 +0900

    :recycle: Refactoring.

--- 034/034.rb
+++ 034/034.rb
@@ -1,3 +1,7 @@
+def sum_of_factorial_of_digits?(n)
+	n.to_s.length > 1 && n == n.to_s.each_char.map { |d| (1..d.to_i).reduce(1, :*) }.reduce(:+)
+end
+
 p (0..50000).select { |i|
-  i.to_s.length > 1 && i == i.to_s.each_char.map { |d| (1..d.to_i).reduce(1, :*) }.reduce(:+)
+	sum_of_factorial_of_digits?(i)
 }.reduce(:+)
