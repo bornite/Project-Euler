37c52514c85667a14c39117956f20ade9e782aa7
bornite <tmkoikee@gmail.com>
2021-01-31 13:20:12 +0900

    :recycle: Refactoring.

--- 035/035.rb
+++ 035/035.rb
@@ -1,3 +1,5 @@
+require 'prime'
+
 def circular(n)
     ret = []
     buf = n
@@ -12,25 +14,13 @@ def circular(n)
     return n.to_s.slice(1, n.to_s.length-1) + n.to_s.slice(0, 1)
   end
   
-  def check_if_prime(n)
-    return true if n == 2
-    return false if n != 2 && n % 2 == 0
-   
-    limit = Math.sqrt(n).ceil
-    3.step(limit, 2) do |i|
-      return false if n % i == 0
-    end
-  
-    return true
-  end
-  
   answer = [2]
   3.step(1000000, 2) do |i|
-    next if !check_if_prime(i)
-    if circular(i).inject(true) { |sum, j| sum = sum && check_if_prime(j.to_i) }
+    next if !i.prime?
+    if circular(i).inject(true) { |sum, j| sum = sum && j.to_i.prime? }
       answer.push(i)
     end
   end
   
-  puts answer.to_s
-  puts answer.length
+  p answer
+  p answer.length
