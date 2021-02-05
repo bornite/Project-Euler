50c48591923bfa50446092359955263e41853cce
bornite <tmkoikee@gmail.com>
2021-02-05 22:51:24 +0900

    :sparkles: Add Ruby code to solve problem 038.

---
+++ 038/038.rb
@@ -0,0 +1,9 @@
+p (1..9999).flat_map { |i|
+  (1..9).map { |j|
+    (1..j).map { |k|
+      i * k
+    }.reduce('') { |s,v| s + v.to_s }
+  }
+}.select { |s|
+  s.length == 9 && s.split('').sort.join('') == '123456789'
+}.map { |s| s.to_i }.max
