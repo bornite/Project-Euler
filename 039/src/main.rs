e55eb43a4720722ff7667c7b798c4b012b6dbb87
bornite <tmkoikee@gmail.com>
2021-02-07 14:36:49 +0900

    :sparkles: Add Rust code to solve problem 039.

---
+++ 039/src/main.rs
@@ -0,0 +1,24 @@
+fn is_pythagorean_number(a: u64, b: u64, c: u64) -> bool {
+	return a*a+b*b == c*c
+}
+
+fn main() {
+    let mut max_solutions = 0;
+    let mut p_max_solutions = 0;
+    'p:for perimeter in 1..1001 {
+        let mut solutions = 0;
+        'a:for a in 1..perimeter/2+1 {
+            'b:for b in a..perimeter/2+1 {
+                let c = perimeter - a - b;
+                if c <= 0 { continue 'a }
+                if is_pythagorean_number(a, b, c) {
+			solutions += 1
+		}
+            }
+        }
+        if solutions > max_solutions {
+            max_solutions = solutions;
+            p_max_solutions = perimeter }
+    }
+    println!("{}", p_max_solutions);
+}
