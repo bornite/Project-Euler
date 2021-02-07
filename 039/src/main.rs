cc4db74bb8e96049bfbdf34404e6bb25fac3cfc2
bornite <tmkoikee@gmail.com>
2021-02-07 14:52:15 +0900

    :recycle: Refactoring.

--- 039/src/main.rs
+++ 039/src/main.rs
@@ -1,24 +1,27 @@
 fn is_pythagorean_number(a: u64, b: u64, c: u64) -> bool {
-	return a*a+b*b == c*c
+	return a*a + b*b == c*c
 }
 
 fn main() {
     let mut max_solutions = 0;
     let mut p_max_solutions = 0;
-    'p:for perimeter in 1..1001 {
+    'p:for perimeter in 2..1001 {
         let mut solutions = 0;
-        'a:for a in 1..perimeter/2+1 {
-            'b:for b in a..perimeter/2+1 {
+        'a:for a in 1..=perimeter/2 {
+            'b:for b in a..=perimeter/2 {
                 let c = perimeter - a - b;
-                if c <= 0 { continue 'a }
+                if c <= 0 {
+			continue 'a;
+		}
                 if is_pythagorean_number(a, b, c) {
-			solutions += 1
+			solutions += 1;
 		}
             }
         }
         if solutions > max_solutions {
             max_solutions = solutions;
-            p_max_solutions = perimeter }
+            p_max_solutions = perimeter
+	}
     }
     println!("{}", p_max_solutions);
 }
