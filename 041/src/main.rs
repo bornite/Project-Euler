c9db9082119134a0bca2d42f3290bbbba48b8574
bornite <tmkoikee@gmail.com>
2021-02-11 16:50:52 +0900

    :sparkles: Add Rust code to solve problem 041.

--- 041/src/main.rs
+++ 041/src/main.rs
@@ -21,8 +21,36 @@ fn get_prime_vec(max_prime: u64) -> Vec<u64> {
     return ret;
 }
 
+fn find_biggest_pandigital_prime(prime_numbers:&[u64]) -> Option<u64> {
+	'primes : for &prime in prime_numbers.iter().rev() {
+		let mut digits_found = [false;9];
+		let prime_string : String = prime.to_string();
+		for ch in prime_string.chars() {
+			let digit:u32 = ch.to_digit(10).unwrap();
+			if digit == 0 {
+                continue 'primes
+            };
+			if !digits_found[(digit-1) as usize] {
+				digits_found[(digit-1) as usize] = true;
+			}
+			else {
+                continue 'primes;
+            }
+		}
+
+		for (n,&digit_present) in digits_found.iter().enumerate() {
+			if !digit_present { continue 'primes }; // failed
+			if n+1 == prime_string.len() { break }; // passed
+		}
+		return Some(prime);
+	}
+	return None;
+}
+
 fn main() {
     let max_prime = 7654321;
     let prime_numbers = get_prime_vec(max_prime);
-    println!("{:?}", prime_numbers);
+    if let Some(biggest_pandigital_prime) = find_biggest_pandigital_prime(&prime_numbers) {
+		println!("{}", biggest_pandigital_prime);
+	}
 }
