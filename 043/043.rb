023784e403f9ec23b408070bdfd1ac2e6e6cd669
bornite <tmkoikee@gmail.com>
2021-02-13 11:23:10 +0900

    :sparkles: Add Ruby code to solve problem 043.

---
+++ 043/043.rb
@@ -0,0 +1,10 @@
+
+primes = [2,3,5,7,11,13,17]
+def check_divisibility(n, primes)
+	(1..7).all? { |i| n[i..i+2].join('').to_i % primes[i-1] == 0
+	}
+end
+
+p ('0'..'9').to_a.permutation.select { |n|
+	check_divisibility(n, primes)
+}.map { |digits| digits.join('').to_i }.sum
