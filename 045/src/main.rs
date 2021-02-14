c215dc89b94df4b9636ca1ac9376509b7c529483
bornite <tmkoikee@gmail.com>
2021-02-14 12:02:26 +0900

    :recycle: Fix warnings.

--- 045/src/main.rs
+++ 045/src/main.rs
@@ -4,21 +4,19 @@ fn hexagon  ( n : u64 ) -> u64 { (4*n*n-2*n)/2 }
 
 fn main() {
 	let triangle_start = 285+1;
-	let mut pentagon_start = 165+1;
-	let mut hexagon_start = 143+1;
+	let pentagon_start = 165+1;
+	let hexagon_start = 143+1;
 	'tri:for tr in triangle_start.. {
 		let triangle_number = triangle(tr);
 		for pen in pentagon_start.. {
 			let pentagon_number = pentagon(pen);
 			if pentagon_number < triangle_number { 
-				//pentagon_start += 1;
 				continue 
 			} else if pentagon_number > triangle_number { break }
 			
 			for hex in hexagon_start.. { // pentagon == triangle here
 				let hexagon_number = hexagon(hex);
 				if hexagon_number < triangle_number {
-					//hexagon_start += 1;
 				} else if hexagon_number > pentagon_number { break }
 				else {
 					println!("{},{},{}:{}", tr, pen, hex, triangle_number);
@@ -26,6 +24,5 @@ fn main() {
 				}
 			}
 		}
-		//if tr % 10000 == 0 { println!("{}", triangle_number/2) }
 	}
 }
