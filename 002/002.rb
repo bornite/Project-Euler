722f2bbd65a573df50cf08e34536dca020e99169
bornite <tmkoikee@gmail.com>
2021-02-15 22:26:40 +0900

    :rotating_light: Add rspec test code for problem 002.

--- 002/002.rb
+++ 002/002.rb
@@ -1,13 +1,17 @@
-def sum_of_even_fibonacci_seq(limit)
-  fib = [1, 2]
-  loop do
-    n = fib[-2..-1].sum
-    break if n > limit
-    fib << n
-  end
-  p fib
-  fib.select{ |n| n%2 == 0 }.sum
-end
+class Problem002
+	def sum_of_even_fibonacci_seq(limit)
+		fib = [1, 2]
+		loop do
+			n = fib[-2..-1].sum
+			break if n > limit
+			fib << n
+		end
+		fib.select{ |n| n%2 == 0 }.sum
+	end
 
-limit = 4_000_000
-p sum_of_even_fibonacci_seq(limit)
+	def compute()
+		limit = 4_000_000
+		sum_of_even_fibonacci_seq(limit)
+	end
+end
+p Problem002.new().compute()
