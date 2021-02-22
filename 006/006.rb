f08f3f0e3d2d16f209fd3d805312d1eff5e3fe3a
bornite <tmkoikee@gmail.com>
2021-02-22 22:36:21 +0900

    :rotating_light: Add RSpec for problem 006.

--- 006/006.rb
+++ 006/006.rb
@@ -1,14 +1,17 @@
-def sum_of_square(n)
-   seq = [*1..n]
-   seq.map{ |i| i**2 }.sum
-end
+class Problem006
+   def sum_of_square(n)
+      seq = [*1..n]
+      seq.map{ |i| i**2 }.sum
+   end
 
-def square_of_sum(n)
-   seq = [*1..n]
-   (seq.sum)**2
-end
+   def square_of_sum(n)
+      seq = [*1..n]
+      (seq.sum)**2
+   end
 
-n = 100
-p sum_of_square(n)
-p square_of_sum(n)
-p square_of_sum(n) - sum_of_square(n)
+   def compute()
+      n = 100
+      square_of_sum(n) - sum_of_square(n)
+   end
+end
+p Problem006.new().compute()
