b40235c392909b41cf6e3b5bcc309cbe40c99c67
bornite <tmkoikee@gmail.com>
2021-02-26 22:49:38 +0900

    :rotating_light: Add RSpec for get_nth_prime_number() method.

--- 007/spec/007_spec.rb
+++ 007/spec/007_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Problem007 do
             expect(Problem007.new().compute()).to eq(104743)
         end
     end
+
+    describe '#get_nth_prime_number' do
+        it 'is get the 6th prime number' do
+            expect(Problem007.new().get_nth_prime_number(6)).to eq(13)
+        end
+    end
 end
