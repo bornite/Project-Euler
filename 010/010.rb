58ed5f13496269494e5b16f96aafdb79b6e8bd37
bornite <tmkoikee@gmail.com>
2021-02-28 13:14:11 +0900

    :rotating_light: Add rspec test code for problem 010.

--- 010/010.rb
+++ 010/010.rb
@@ -1,15 +1,23 @@
 require 'prime'
 
-primes = []
-gen = Prime::EratosthenesGenerator.new
-loop do
-  p = gen.next
-  if p <= 2_000_000
-     primes.push(p)
-  else
-     break
+class Problem010
+  def get_primes(n)
+    primes = []
+    gen = Prime::EratosthenesGenerator.new
+    loop do
+      p = gen.next
+      if p <= n
+         primes.push(p)
+      else
+         break
+      end
+    end
+    primes
+  end
+
+  def compute()
+    primes = get_primes(2_000_000)
+    primes.sum
   end
 end
-
-p primes.sum
-
+p Problem010.new().compute()
