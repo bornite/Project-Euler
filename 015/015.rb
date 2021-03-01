845768a6321eceb14e22dea14298cb12df99790f
bornite <tmkoikee@gmail.com>
2021-03-01 22:04:52 +0900

    :hammer: Refactor code

--- 015/015.rb
+++ 015/015.rb
@@ -1,16 +1,21 @@
-def factorial(n)
-    return 1 if n.zero?
-    (1..n).inject(:*)
-end
- 
-def combination(n, r)
-   return 1 if r.zero?
-   (n - r + 1..n).inject(:*) / factorial(r)
-end
+class Problem015
+  def factorial(n)
+      return 1 if n.zero?
+      (1..n).inject(:*)
+  end
 
-def permutation(n, r)
-   return 1 if r.zero?
-   (n - r + 1..n).inject(:*)
-end
+  def combination(n, r)
+     return 1 if r.zero?
+     (n - r + 1..n).inject(:*) / factorial(r)
+  end
 
-p combination(40, 20)
+  def permutation(n, r)
+     return 1 if r.zero?
+     (n - r + 1..n).inject(:*)
+  end
+
+  def compute()
+    combination(40, 20)
+  end
+end
+p Problem015.new().compute()
