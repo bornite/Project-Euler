aedbb6a3e9b2a2c15e36af93499e53835cc4f956
bornite <tmkoikee@gmail.com>
2021-03-04 16:36:55 +0900

    :sparkles: Add Rust code to solve problem 056.

---
+++ 056/src/main.rs
@@ -0,0 +1,32 @@
+extern crate num_bigint;
+extern crate num_traits;
+
+use num_bigint::BigInt;
+use num_traits::Pow;
+
+fn sum_of_digits(num: String) -> u64 {
+    let mut sum: u64 = 0;
+    for i in num.chars() {
+        sum = sum + (i as u64 - 48);
+    }
+    println!("sum = {}", sum);
+    return sum;
+}
+
+fn main() {
+    let mut max: u64 = 0;
+    let mut sum: u64 = 0;
+    for a in 1..=100 {
+        for b in 1..=100 {
+            let x: BigInt = a.into();
+            let ret = x.pow(b as u32);
+            println!("a^b = {}", ret);
+            println!("str = {}", ret.to_str_radix(10));
+            sum = sum_of_digits(ret.to_str_radix(10));
+            if sum > max {
+                max = sum;
+            }
+        }
+    }
+    println!("{}", max);
+}
