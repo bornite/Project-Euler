85b15f056d5e344c16b210c1eb1e2e3ee362fbbb
bornite <tmkoikee@gmail.com>
2021-03-13 17:01:19 +0900

    :heavy_check_mark: Fix test code.

--- 060/spec/060_spec.rb
+++ 060/spec/060_spec.rb
@@ -3,7 +3,7 @@ require "./060"
 RSpec.describe Problem060 do
   describe '#compute' do
     it 'is solve Problem 060' do
-      expect(Problem060.new().compute()).to eq(129448)
+      expect(Problem060.new().compute()).to eq(26033)
     end
   end
 end
