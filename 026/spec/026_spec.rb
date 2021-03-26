4ad9223a37788ade53d6e92c856ba8fcc46e169d
bornite <tmkoikee@gmail.com>
2021-03-26 20:00:49 +0900

    :rotating_light: Add a test for to_rec_decimal().

--- 026/spec/026_spec.rb
+++ 026/spec/026_spec.rb
@@ -1,6 +1,13 @@
 require "./026"
 
 RSpec.describe Problem026 do
+    describe '#to_rec_decimal' do
+        it 'calculate Recurring-Decimals' do
+            expect(Rational(1, 3).to_rec_decimal()).to eq("0.(3)")
+            expect(Rational(1, 7).to_rec_decimal()).to eq("0.(142857)")
+        end
+    end
+
     describe '#compute' do
         it 'is solve Problem 026' do
             expect(Problem026.new().compute()).to eq(983)
