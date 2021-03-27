9599785478ea111fdca22e3f983481803fa7af1a
bornite <tmkoikee@gmail.com>
2021-03-27 17:45:08 +0900

    :recycle: Refactor code for Problem027.

--- 027/027.rb
+++ 027/027.rb
@@ -1,11 +1,16 @@
 require 'prime'
 
-def polynomial(a, b, n) = (n.to_i)**2 + (a.to_i)*(n.to_i) + b.to_i
+class Problem027
+	def polynomial(a, b, n) = (n.to_i)**2 + (a.to_i)*(n.to_i) + b.to_i
 
-a = (-999..999)
-b = (-999..999)
-result = a.to_a.product(b.to_a).map {|a, b|
-	[(0..100).take_while { |n| polynomial(a, b, n).prime? }.count, a , b]
-}.max
-p result
-p result[1].to_i * result[2].to_i
+	def compute()
+		a = (-999..999)
+		b = (-999..999)
+		result = a.to_a.product(b.to_a).map {|a, b|
+			[(0..100).take_while { |n| polynomial(a, b, n).prime? }.count, a , b]
+		}.max
+		p result
+		result[1].to_i * result[2].to_i
+	end
+end
+p Problem027.new().compute()
