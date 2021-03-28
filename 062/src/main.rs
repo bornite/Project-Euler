e7df9cbca181a576e56a2900b12e778187834125
bornite <tmkoikee@gmail.com>
2021-03-28 10:53:46 +0900

    :zap: Implement loop processing.

--- 062/src/main.rs
+++ 062/src/main.rs
@@ -1,11 +1,14 @@
 extern crate itertools;
-use itertools::Itertools;
+use std::collections::HashMap;
 
-fn gen_cubic_numbers(limit: usize) -> Vec<i128> {
+fn gen_cubic_numbers(lower_limit: usize, upper_limit: usize) -> Vec<i128> {
     let mut vec = Vec::new();
     for i in 1.. {
         let n = (i as i128).pow(3);
-        if n.to_string().chars().count() > limit {
+        if n.to_string().chars().count() < lower_limit {
+            continue;
+        }
+        if n.to_string().chars().count() > upper_limit {
             break;
         }
         vec.push(n);
@@ -14,15 +17,29 @@ fn gen_cubic_numbers(limit: usize) -> Vec<i128> {
 }
 
 fn main() {
-    let mut cubic_numbers = Vec::new();
-    cubic_numbers = gen_cubic_numbers(4);
+    let cubic_numbers: Vec<i128> = gen_cubic_numbers(11, 12);
+    let mut result: HashMap<String, Vec<i128>> = HashMap::new();
+
     for c in cubic_numbers {
-        let mut digits: Vec<i32> = c.to_string().chars().map(|c| c as i32 - 48).collect();
-        println!("{:?}", digits);
-        let len_digits = digits.len();
-        let it = digits.into_iter().permutations(4);
-        for i in it {
-            println!("{:?}", i);
+        println!("c: {}", c);
+        let mut digits: Vec<char> = c.to_string().chars().collect();
+        println!("digits: {:?}", digits);
+        let mut s: Vec<String> = digits.iter().map(|d| d.to_string()).collect();
+        s.sort();
+        let key: String = s.join("");
+        println!("key: {}", key);
+        if result.contains_key(&key) {
+            println!("contains_key:yes");
+            let mut t: Vec<i128> = result.get(&key).unwrap().to_vec();
+            t.push(c);
+            result.insert(key, t);
+        }
+        else {
+            println!("not contains_key:no");
+            let mut t: Vec<i128> = Vec::new();
+            t.push(c);
+            result.insert(key, t);
         }
     }
+    println!("result:{:?}", result);
 }
