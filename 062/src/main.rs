962d24a89df0e1915e8b871de964b722382648db
bornite <tmkoikee@gmail.com>
2021-03-28 11:14:17 +0900

    :fire: Delete debug print.

--- 062/src/main.rs
+++ 062/src/main.rs
@@ -21,25 +21,24 @@ fn main() {
     let mut result: HashMap<String, Vec<i128>> = HashMap::new();
 
     for c in cubic_numbers {
-        println!("c: {}", c);
         let mut digits: Vec<char> = c.to_string().chars().collect();
-        println!("digits: {:?}", digits);
         let mut s: Vec<String> = digits.iter().map(|d| d.to_string()).collect();
         s.sort();
         let key: String = s.join("");
-        println!("key: {}", key);
         if result.contains_key(&key) {
-            println!("contains_key:yes");
             let mut t: Vec<i128> = result.get(&key).unwrap().to_vec();
             t.push(c);
             result.insert(key, t);
         }
         else {
-            println!("not contains_key:no");
             let mut t: Vec<i128> = Vec::new();
             t.push(c);
             result.insert(key, t);
         }
     }
-    println!("result:{:?}", result);
+    for (k, v) in &result {
+        if v.len() == 5 {
+            println!("{}: {:?}", k, v);
+        }
+    }
 }
