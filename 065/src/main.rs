2f5c290f60bd55e45521afad09eb34a286e0ac20
bornite <tmkoikee@gmail.com>
2021-04-05 07:53:06 +0900

    :crab: Add Rust code to solve problem 065.

---
+++ 065/src/main.rs
@@ -0,0 +1,40 @@
+extern crate num_bigint;
+extern crate num_traits;
+
+use num_bigint::BigUint;
+
+fn get_e_seq(i: u64) -> u64 {
+    match i {
+        0 => 2u64,
+        i if i % 3 == 0 => (2 * (i / 3)),
+        _ => 1,
+    }
+}
+
+fn sum_of_digits(n: BigUint) -> u32 {
+    n.to_string()
+    .chars()
+    .filter_map(|c| c.to_digit(10))
+    .sum::<u32>()
+}
+
+fn main() {
+    let upper_bound: usize = 100;
+    let mut result: u32 = 0u32;
+    let mut d: BigUint = 1u64.into();
+    let mut n: BigUint = 2u64.into();
+
+    for i in 2..=upper_bound {
+        let temp_d = d;
+        let temp_n = n;
+        
+        let c: u64 = get_e_seq(i as u64);
+
+        println!("i:{}, c:{}", i, c);
+        d = temp_n.clone();
+        n = c * temp_n.clone() + temp_d.clone();
+        println!("d:{}, n:{}", d, n);
+    }
+    result = sum_of_digits(n);
+    println!("{}", result);
+}
