6f424af95f0871aa3785dddca6fc87361d7671b4
bornite <tmkoikee@gmail.com>
2021-04-07 23:09:47 +0900

    :hammer: Refactor code for Problem 030.

--- 030/030.rb
+++ 030/030.rb
@@ -1,6 +1,10 @@
-def sum_of_digit_fifth_powers(n)
-	n.to_s.chars.map {|s| (s.to_i)**5 }.sum
-end
+class Problem030
+   def sum_of_digit_fifth_powers(n)
+      n.to_s.chars.map {|s| (s.to_i)**5 }.sum
+   end
 
-nums = (2..999999).select {|n| n == sum_of_digit_fifth_powers(n) }
-p nums.sum
+   def compute()
+     nums = (2..999999).select {|n| n == sum_of_digit_fifth_powers(n) }
+     nums.sum
+   end
+end
