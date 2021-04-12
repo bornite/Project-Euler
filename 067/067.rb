e994b3c40b43477fb5f07f63547b2ab04820ce8a
bornite <tmkoikee@gmail.com>
2021-04-12 20:05:47 +0900

    :sparkles: Add Ruby code to solve problem 067.

---
+++ 067/067.rb
@@ -0,0 +1,17 @@
+def find_path(triangle)
+    # Search for the path from the bottom and second from the bottom.
+    (triangle.length - 2).downto(0) do |y|
+       triangle[y].length.times do |x|
+          triangle[y][x] += [triangle[y+1][x], triangle[y+1][x+1]].max
+       end
+    end
+    triangle[0][0]
+end
+ 
+data = Array.new()
+File.open("triangle.txt") do |file|
+  file.each_line do |line|
+    data.push(line.split(" ").map(&:to_i))
+  end
+end
+p find_path(data)
