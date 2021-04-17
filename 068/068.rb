2b7d2df6529fd3a63118890fb775cea107733166
bornite <tmkoikee@gmail.com>
2021-04-17 17:09:12 +0900

    :pencil: Refactoring code.

--- 068/068.rb
+++ 068/068.rb
@@ -5,7 +5,8 @@ class Problem068
     len.times do |i|
       str += " #{outer[i]},#{inner[i]},#{inner[(i+1)%len]};"
     end
-    p sum.to_s + str + ' ' + str.gsub(/[,; ]/, '')
+    #p sum.to_s + str + ' ' + str.gsub(/[,; ]/, '')
+    sum.to_s + str + ' ' + str.gsub(/[,; ]/, '')
   end
   
   def check(inner, outer)
@@ -13,7 +14,7 @@ class Problem068
     len = inner.length
     len.times do |i|
       sum = outer[i] + inner[i] + inner[(i+1)%len]
-      return if arr.length != 0 && arr.last != sum
+      return "" if arr.length != 0 && arr.last != sum
       arr.push(sum)
     end
     output(inner, outer, arr.last)
@@ -31,14 +32,19 @@ class Problem068
     max = 10
     nums = [*1..max]
     outers = nums.combination(max/2).to_a
+    answer = 0
     
     outers.each do |raw_outer|
       outer = create_outer(raw_outer)
       inners = (nums.dup - outer).permutation(max/2)
       inners.each do |inner|
-        check(inner, outer)
+        check_data = check(inner, outer).split()
+        if check_data[6] != nil && check_data[6].size == 16 && check_data[6].to_i  > answer
+          answer = check_data[6].to_i
+        end
       end 
     end
+    answer
   end
 end
-Problem068.new().compute()
+p Problem068.new().compute()
