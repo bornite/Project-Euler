1d945469e4cbf23c9e929ce1e83873b2c3ddd1b5
bornite <tmkoikee@gmail.com>
2021-04-17 17:34:19 +0900

    :hammer: Refactor code.

--- 067/067.rb
+++ 067/067.rb
@@ -1,17 +1,22 @@
-def find_path(triangle)
-    # Search for the path from the bottom and second from the bottom.
-    (triangle.length - 2).downto(0) do |y|
-       triangle[y].length.times do |x|
-          triangle[y][x] += [triangle[y+1][x], triangle[y+1][x+1]].max
-       end
+class Problem067
+    def find_path(triangle)
+        # Search for the path from the bottom and second from the bottom.
+        (triangle.length - 2).downto(0) do |y|
+           triangle[y].length.times do |x|
+              triangle[y][x] += [triangle[y+1][x], triangle[y+1][x+1]].max
+           end
+        end
+        triangle[0][0]
     end
-    triangle[0][0]
+     
+    def compute()
+        data = Array.new()
+        File.open("triangle.txt") do |file|
+          file.each_line do |line|
+            data.push(line.split(" ").map(&:to_i))
+          end
+        end
+        find_path(data)
+    end
 end
- 
-data = Array.new()
-File.open("triangle.txt") do |file|
-  file.each_line do |line|
-    data.push(line.split(" ").map(&:to_i))
-  end
-end
-p find_path(data)
+p Problem067.new().compute()
