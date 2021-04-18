f6d613d7aa6f2f96af39fb02b6d94f5ed936b9dc
bornite <tmkoikee@gmail.com>
2021-04-18 12:09:12 +0900

    :hammer: Refactor Ruby code for Problem 069.

--- 069/069.rb
+++ 069/069.rb
@@ -1,29 +1,34 @@
 require 'prime'
 
-def totient(n)
-    return n-1 if n.prime?
-  
-    ret = n
-    prime_factors = n.prime_division()
-    prime_factors.each do |p|
-      ret = ret*(p[0]-1)/p[0]
-    end
-    ret
-end
+class Problem069
+    def totient(n)
+        return n-1 if n.prime?
 
-def n_divided_by_totient(n)
-    return n.to_f/totient(n)
-end
+        ret = n
+        prime_factors = n.prime_division()
+        prime_factors.each do |p|
+        ret = ret*(p[0]-1)/p[0]
+        end
+        ret
+    end
 
-limit = 1_000_000
-answer = 0
-max = 0
-2.upto(1_000_000) do |n|
-    result = n_divided_by_totient(n)
-    if result > max
-        max = result
-        answer = n
-        puts "n:#{n} max:#{max}"
+    def n_divided_by_totient(n)
+        return n.to_f/totient(n)
+    end
+
+    def compute()
+        limit = 1_000_000
+        answer = 0
+        max = 0
+        2.upto(1_000_000) do |n|
+            result = n_divided_by_totient(n)
+            if result > max
+                max = result
+                answer = n
+                puts "n:#{n} max:#{max}"
+            end
+        end
+        answer
     end
 end
-p answer
+p Problem069.new().compute()
