053714135a9c926bd9091746e4e3f32754831409
bornite <tmkoikee@gmail.com>
2021-04-30 19:17:09 +0900

    :hammer: Refactor Ruby code for Problem 034.

--- 034/034.rb
+++ 034/034.rb
@@ -1,7 +1,12 @@
-def sum_of_factorial_of_digits?(n)
-	n.to_s.length > 1 && n == n.to_s.each_char.map { |d| (1..d.to_i).reduce(1, :*) }.reduce(:+)
+class Problem034
+  def sum_of_factorial_of_digits?(n)
+  	n.to_s.length > 1 && n == n.to_s.each_char.map { |d| (1..d.to_i).reduce(1, :*) }.reduce(:+)
+  end
+  
+  def compute()
+    (0..50000).select { |i|
+    	sum_of_factorial_of_digits?(i)
+    }.reduce(:+)
+  end
 end
-
-p (0..50000).select { |i|
-	sum_of_factorial_of_digits?(i)
-}.reduce(:+)
+p Problem034.new().compute()
