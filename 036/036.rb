e327c7dc4ae07d74456f8a2a60694f10a139511b
bornite <tmkoikee@gmail.com>
2021-05-01 13:52:55 +0900

    :hammer: Refactor Ruby code for Problem 036.

--- 036/036.rb
+++ 036/036.rb
@@ -1,5 +1,10 @@
-def palindromic?(n)
+class Problem036
+    def palindromic?(n)
         n.to_s == n.to_s.reverse
+    end
+    
+    def compute()
+        (1..1_000_000).to_a.select {|x| palindromic?(x) && palindromic?(x.to_s(2)) }.sum
+    end
 end
-
-p (1..1_000_000).to_a.select {|x| palindromic?(x) && palindromic?(x.to_s(2)) }.sum
+p Problem036.new().compute()
