8b89a29a185cada0b48463d43c1cdd13c9821718
bornite <tmkoikee@gmail.com>
2021-05-01 12:43:14 +0900

    :rotating_light: Add rspec test code.

--- 035/spec/035_spec.rb
+++ 035/spec/035_spec.rb
@@ -1,6 +1,11 @@
 require "./035"
 
 RSpec.describe Problem035 do
+    describe '#rotate' do
+        it 'calculate the 197 circulated numbers' do
+            expect(Problem035.new().rotate(197)).to eq("971")
+        end
+    end
     describe '#compute' do
         it 'is solve Problem 035' do
             expect(Problem035.new().compute()).to eq(55)
