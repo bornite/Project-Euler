f13bda17d1ba841eb9e8f52d2aabf3c19cf526bd
bornite <tmkoikee@gmail.com>
2021-05-03 16:15:23 +0900

    :hammer: Refactor Ruby code for Problem 038.

--- 038/038.rb
+++ 038/038.rb
@@ -1,12 +1,17 @@
-pandigital_multiples = (1..9999).flat_map { |i|
-  (1..9).map { |j|
-    (1..j).map { |k|
-      i * k
-    }.reduce('') { |s,v| s + v.to_s }
-  }
-}.select { |s|
-  s.length == 9 && s.split('').sort.join('') == '123456789'
-}
+class Problem038
+  def compute()
+    pandigital_multiples = (1..9999).flat_map { |i|
+      (1..9).map { |j|
+        (1..j).map { |k|
+          i * k
+        }.reduce('') { |s,v| s + v.to_s }
+      }
+    }.select { |s|
+      s.length == 9 && s.split('').sort.join('') == '123456789'
+    }
 
-p pandigital_multiples
-p pandigital_multiples.map { |s| s.to_i }.max
+    p pandigital_multiples
+    pandigital_multiples.map { |s| s.to_i }.max
+  end
+end
+p Problem038.new().compute()
