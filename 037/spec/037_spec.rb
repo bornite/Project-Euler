9292fb7d36bd4b6e519a6bddbd2fc12ab961992f
bornite <tmkoikee@gmail.com>
2021-05-03 16:09:33 +0900

    :rotating_light: Add rspec test code for trancatable_l() and trancatable_r()

--- 037/spec/037_spec.rb
+++ 037/spec/037_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Problem037 do
             expect(Problem037.new().compute()).to eq(748317)
         end
     end
+    describe '#trancatable_l' do
+        it 'check if a number is prime by remove digit from left to right' do
+            expect(Problem037.new().trancatable_l("3797")).to eq(true)
+        end
+    end
+    describe '#trancatable_r' do
+        it 'check if a number is prime by remove digit from right to left to' do
+            expect(Problem037.new().trancatable_l("3797")).to eq(true)
+        end
+    end
 end
