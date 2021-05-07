630513974b6d8c9dc0e2c56b5332562a7d4a3419
bornite <tmkoikee@gmail.com>
2021-05-07 18:24:35 +0900

    :hammer: Refactor Ruby code for Problem 041.

--- 041/041.rb
+++ 041/041.rb
@@ -1,7 +1,12 @@
 require 'prime'
 
-puts (1..9).flat_map { |n|
-	('1'..n.to_s).to_a.permutation.map { |p|
-		p.join('').to_i
-	}.select { |i| i.prime? }
-}.max
+class Problem041
+  def compute()
+    (1..9).flat_map { |n|
+        ('1'..n.to_s).to_a.permutation.map { |p|
+            p.join('').to_i
+        }.select { |i| i.prime? }
+    }.max
+  end
+end
+p Problem041.new().compute()
