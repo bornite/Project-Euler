07d57a4884c46cec8368a644563b0cda1a9eae01
bornite <tmkoikee@gmail.com>
2021-05-07 17:28:22 +0900

    :pencil: Update 071.rb

--- 071/071.rb
+++ 071/071.rb
@@ -1,19 +1,41 @@
-require 'set'
-
 class Problem071
-    def compute()
-        s = Set.new()
-        n = (1..999_999).to_a
-        d = (1..1_000_000).to_a
-        n.product(d) {|x, y|
-            if x < y
-                r = Rational(x, y)
-                s.add(r)
-            end
-        }
-        array_of_fracs = s.sort.to_a
-        i = array_of_fracs.find_index(Rational(3, 7))
-        array_of_fracs[i - 1].numerator
+  def hcf(n, m)
+    if m < n
+      buf = m
+      m = n
+      n = buf
     end
+    return n if m%n == 0
+    hcf(m%n, n)
+  end
+
+  def compare_with_frac(base_frac, frac)
+    n = frac[0]*base_frac[1]
+    compared_n = base_frac[0]*frac[1]
+
+    return 0 if n == compared_n
+    n-compared_n > 0 ? 1 : -1
+  end
+
+  def compute()
+    max = 1000000
+    answer_div = max
+    answer = [1,1]
+    (max/7-1).upto(max/7) do |d_div|
+      (d_div*7).upto(d_div*7+6) do |d|
+        break if d > max
+        (d_div*2+1).upto(d_div*3-1) do |n|
+          # p "#{n}/#{d}"
+          if 3/7.0-n/d.to_f < answer_div
+            answer_div = 3/7.0-n/d.to_f
+            answer = [n,d]
+            # p "answer_div = #{answer_div}, answer = #{answer}"
+          end
+          break if compare_with_frac([n,d], [3,7]) < 0
+        end
+      end
+    end
+    answer
+  end
 end
 p Problem071.new().compute()
