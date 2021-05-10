e021085cb64a2e3953a2713e096b64bc49cb7e19
bornite <tmkoikee@gmail.com>
2021-05-10 20:24:22 +0900

    :pencil: Fix test.

--- 041/spec/041_spec.rb
+++ 041/spec/041_spec.rb
@@ -3,7 +3,7 @@ require "./041"
 RSpec.describe Problem041 do
     describe '#compute' do
         it 'is solve Problem 041' do
-            expect(Problem041.new().compute()).to eq(210)
+            expect(Problem041.new().compute()).to eq(7652413)
         end
     end
 end
