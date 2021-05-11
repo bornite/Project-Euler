01ddc57ac790c099b82ec338bf9871086c97d6c3
bornite <tmkoikee@gmail.com>
2021-05-11 19:55:43 +0900

    :hammer: Refactor Ruby code for Problem 042

--- 042/042.rb
+++ 042/042.rb
@@ -1,9 +1,14 @@
-def triangle_word?(word, triangle_numbers)
-	word_value = word.each_byte.reduce(0){|s,v| s+(v-64)}
-	triangle_numbers.include?(word_value)
-end
+class Problem042
+	def triangle_word?(word, triangle_numbers)
+		word_value = word.each_byte.reduce(0){|s,v| s+(v-64)}
+		triangle_numbers.include?(word_value)
+	end
 
-triangle_numbers = (1..1000).map { |n| (n*(n+1))/2 }.to_a
-p File.read(File.dirname(__FILE__) + '/words.txt').scan(/\w+/).select { |word|
-	triangle_word?(word, triangle_numbers)
-}.count
+	def compute()
+		triangle_numbers = (1..1000).map { |n| (n*(n+1))/2 }.to_a
+		File.read(File.dirname(__FILE__) + '/words.txt').scan(/\w+/).select { |word|
+			triangle_word?(word, triangle_numbers)
+		}.count
+	end
+end
+p Problem042.new().compute()
