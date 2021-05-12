1bc0c6618d1cd3b99e4f8a4d88b735716393581c
bornite <tmkoikee@gmail.com>
2021-05-12 17:32:46 +0900

    :hammer: Refactor Ruby code for Problem 043.

--- 043/043.rb
+++ 043/043.rb
@@ -1,10 +1,14 @@
+class Problem043
+	def check_divisibility(n, primes)
+		(1..7).all? { |i| n[i..i+2].join('').to_i % primes[i-1] == 0
+		}
+	end
 
-primes = [2,3,5,7,11,13,17]
-def check_divisibility(n, primes)
-	(1..7).all? { |i| n[i..i+2].join('').to_i % primes[i-1] == 0
-	}
+	def compute()
+		primes = [2,3,5,7,11,13,17]
+		('0'..'9').to_a.permutation.select { |n|
+			check_divisibility(n, primes)
+		}.map { |digits| digits.join('').to_i }.sum
+	end
 end
-
-p ('0'..'9').to_a.permutation.select { |n|
-	check_divisibility(n, primes)
-}.map { |digits| digits.join('').to_i }.sum
+p Problem043.new().compute()
