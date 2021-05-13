5ee2d21cda1824fec665c3815fdc3f34d645a8bb
bornite <tmkoikee@gmail.com>
2021-05-13 22:28:04 +0900

    :hammer: Refactor Ruby code for Problem 044.

--- 044/044.rb
+++ 044/044.rb
@@ -1,6 +1,12 @@
 require 'set'
 
-pentagonals = (1..10000).map { |n| n*(3*n-1)/2 }.to_set
-p pentagonals.to_a.combination(2).select { |n,m|
-    pentagonals.include?(n+m) && pentagonals.include?((n-m).abs)
-}.map { |a,b| (a-b).abs }.min
+class Problem044
+
+  def compute()
+    pentagonals = (1..10000).map { |n| n*(3*n-1)/2 }.to_set
+    pentagonals.to_a.combination(2).select { |n,m|
+        pentagonals.include?(n+m) && pentagonals.include?((n-m).abs)
+    }.map { |a,b| (a-b).abs }.min
+  end
+end
+p Problem044.new().compute()
