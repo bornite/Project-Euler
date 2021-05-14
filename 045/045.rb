e5eae4960ad0c7381818a4722853364e72874554
bornite <tmkoikee@gmail.com>
2021-05-14 18:10:06 +0900

    :hammer: Refactor Ruby code for Problem 045.

--- 045/045.rb
+++ 045/045.rb
@@ -1,8 +1,13 @@
 require 'set'
 
-p = (1..100000).map { |n| n*(3*n-1)/2 }.to_set
-h = (1..100000).map { |n| n*(2*n-1) }.to_set
-
-p (286..100000).map { |n| n*(n+1)/2 }.detect { |t|
-  p.include?(t) and h.include?(t)
-}
+class Problem045
+  def compute()
+    p = (1..100000).map { |n| n*(3*n-1)/2 }.to_set
+    h = (1..100000).map { |n| n*(2*n-1) }.to_set
+    
+    (286..100000).map { |n| n*(n+1)/2 }.detect { |t|
+      p.include?(t) and h.include?(t)
+    }
+  end
+end
+p Problem045.new().compute()
