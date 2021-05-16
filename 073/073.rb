fe002ab432ad1959f7cc9de2956a6ba28d1b5f0e
bornite <tmkoikee@gmail.com>
2021-05-16 16:13:39 +0900

    :recycle: fix indent.

--- 073/073.rb
+++ 073/073.rb
@@ -1,26 +1,25 @@
 class Problem073
-  def hcf(n,m)
-    # n < mを保障
-    if m < n
-      buf = n
-      n = m
-      m = buf
+    def hcf(n,m)
+        if m < n
+            buf = n
+            n = m
+            m = buf
+        end
+        return n if m % n == 0
+        hcf(m%n, n)
     end
-    return n if m%n == 0
-    hcf(m%n, n)
-  end
 
-  def compute()
-    max = 12000
-    answer = 0
-    (4..max).each { |d|
-        (d/3.0).ceil.upto((d/2.0).floor) do |n|
-        if hcf(n, d) == 1
-            answer += 1
+    def compute()
+        max = 12_000
+        answer = 0
+        (4..max).each do |d|
+            (d/3.0).ceil.upto((d/2.0).floor) do |n|
+                if hcf(n, d) == 1
+                    answer += 1
+                end
+            end
         end
-        end
-    }
-    answer
-  end
+        answer
+    end
 end
 p Problem073.new().compute()
