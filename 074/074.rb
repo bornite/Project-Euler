5fe01a1bbfb30a9d6c1eed8493c18d6ce52981c3
bornite <tmkoikee@gmail.com>
2021-05-19 18:51:23 +0900

    :hammer: Refactor Ruby code for Problem 074.

--- 074/074.rb
+++ 074/074.rb
@@ -4,29 +4,27 @@ class Problem074
         n*factorial(n-1)
     end
     
-    def sum_of_factorial_of_each_digit(n)
-        n.to_s.split('').map(&:to_i).map{ |m| @fac[m] }.inject(&:+)
+    def sum_of_factorial_of_each_digit(n, fac)
+        n.to_s.split('').map(&:to_i).map{ |m| fac[m] }.inject(&:+)
     end
     
     def compute()
-        @fac = [*0..9].map{ |n| factorial(n) }
+        fac = [*0..9].map{ |n| factorial(n) }
         h = {}
-        @fac.each_with_index{ |n, i| h[i] = n }
-        
+        fac.each_with_index{ |n, i| h[i] = n }
+
         answer = 0
-        
         10.upto(999999) do |n|
             chain = [n]
             m = n
             loop do
-            h[m] = sum_of_factorial_of_each_digit(m) unless h.has_key?(m)
-            m = h[m]
-            break if chain.include?(m)
-            chain.push(m)
+                h[m] = sum_of_factorial_of_each_digit(m, fac) unless h.has_key?(m)
+                m = h[m]
+                break if chain.include?(m)
+                chain.push(m)
             end
             if chain.length == 60
-            answer += 1
-            # p chain
+                answer += 1
             end
         end
         answer
