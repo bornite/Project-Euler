8679d799e8dc9d35e468ad8fb4c15e708daa7600
bornite <tmkoikee@gmail.com>
2021-05-21 23:37:20 +0900

    :rotating_light: Add rspec test code for problem 075.

--- 075/spec/075_spec.rb
+++ 075/spec/075_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Problem075 do
       expect(Problem075.new().compute()).to eq (161667)
     end
   end
+
+  describe '#hcf' do
+    it 'calculate the highest common factor of 6 and 15' do
+      expect(Problem075.new().hcf(6,15)).to eq (3)
+    end
+  end
 end
