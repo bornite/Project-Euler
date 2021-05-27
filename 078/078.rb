3c46a38ca86e27607ec807aa8fa2f7fc21a8d4a1
bornite <tmkoikee@gmail.com>
2021-05-27 23:29:09 +0900

    :hammer: Refactor Ruby code for Problem 078.

--- 078/078.rb
+++ 078/078.rb
@@ -1,30 +1,35 @@
-def pentagonal_under(n)
-  ret = []
-  i = 1
-  loop do
-    break if (negative = i*(3*i-1)/2) >= n
-    ret << negative
-    break if (positive = i*(3*i+1)/2) >= n
-    ret << positive    
-    i += 1
+class Problem078
+  def pentagonal_under(n)
+    ret = []
+    i = 1
+    loop do
+      break if (negative = i*(3*i-1)/2) >= n
+      ret << negative
+      break if (positive = i*(3*i+1)/2) >= n
+      ret << positive
+      i += 1
+    end
+    ret
   end
-  ret
-end
 
-n = 2
-answer = 0
-p = [1, 1]
-pentagonal = pentagonal_under(100000)
-loop do
-  p[n] = 0
-  pentagonal.each_with_index do |penta, i|
-    break if n-penta < 0
-    sign = (i%4 <= 1 ? 1 : -1)
-    p[n] += sign*p[n-penta]
+  def compute()
+    n = 2
+    answer = 0
+    p = [1, 1]
+    pentagonal = pentagonal_under(100_000)
+    loop do
+      p[n] = 0
+      pentagonal.each_with_index do |penta, i|
+        break if n-penta < 0
+        sign = (i%4 <= 1 ? 1 : -1)
+        p[n] += sign*p[n-penta]
+      end
+      p[n] = p[n] % 1_000_000
+      break if p[n] == 0
+      n += 1
+      break if n > pentagonal.max
+    end
+    n
   end
-  p[n] = p[n] % 1000000
-  break if p[n] == 0
-  n += 1
-  break if n > pentagonal.max
 end
-p n
+p Problem078.new().compute()
