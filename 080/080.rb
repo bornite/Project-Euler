a0fe4d936cd285ac67795371ff3802cfc1b5b626
bornite <tmkoikee@gmail.com>
2021-06-03 15:30:25 +0900

    :hammer: Refactor Ruby code for Problem 080.

--- 080/080.rb
+++ 080/080.rb
@@ -1,13 +1,17 @@
 require 'bigdecimal'
 require 'bigdecimal/math'
 
-def square?(n)
-  (Math.sqrt(n).floor)**2 == n
-end
+class Problem080
+  def square?(n)
+    (Math.sqrt(n).floor)**2 == n
+  end
 
-sum = 0
-2.upto(99) do |i|
-  next if square?(i)
-  sum += BigMath::sqrt(BigDecimal(i), 90).to_s[2..-3][0..99].split('').map(&:to_i).inject(:+)
+  def compute()
+    sum = 0
+    2.upto(99) do |i|
+      next if square?(i)
+      sum += BigMath::sqrt(BigDecimal(i), 90).to_s[2..-3][0..99].split('').map(&:to_i).inject(:+)
+    end
+    sum
+  end
 end
-p sum
