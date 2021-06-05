66d5dbe98706671e5ac49b865312b3ded4cf764e
bornite <tmkoikee@gmail.com>
2021-06-05 15:54:34 +0900

    :hammer: Refactor Ruby code for Problem 081.

--- 081/081.rb
+++ 081/081.rb
@@ -1,6 +1,28 @@
-class Problem081
-  def compute()
-    "compute()"
+matrix = []
+File.open("matrix.txt") do |f|
+  f.each_line{ |line| matrix << line.split(',').map(&:to_i) }
+end
+# p matrix
+
+answer = []
+matrix.each_with_index do |row, i|
+  answer[i] = []
+  if i == 0
+    row.each_with_index do |elm, j|
+      if j == 0
+        answer[i][j] = matrix[i][j]
+      else
+        answer[i][j] = answer[i][j-1] + matrix[i][j]
+      end
+    end
+  else
+    row.each_with_index do |elm, j|
+      if j == 0
+        answer[i][j] = answer[i-1][j] + matrix[i][j]
+      else
+        answer[i][j] = [answer[i][j-1], answer[i-1][j]].min + matrix[i][j]
+      end
+    end
   end
 end
-p Problem081.new().compute()
+p answer[-1][-1]
