b5d3c99593959d1cbaefe30d88d60fd70907482b
bornite <tmkoikee@gmail.com>
2021-06-05 16:44:30 +0900

    :hammer: Refactor Ruby code for Problem 081.

--- 081/081.rb
+++ 081/081.rb
@@ -1,28 +1,31 @@
-matrix = []
-File.open("matrix.txt") do |f|
-  f.each_line{ |line| matrix << line.split(',').map(&:to_i) }
-end
-# p matrix
-
-answer = []
-matrix.each_with_index do |row, i|
-  answer[i] = []
-  if i == 0
-    row.each_with_index do |elm, j|
-      if j == 0
-        answer[i][j] = matrix[i][j]
+class Problem081
+  def compute()
+    matrix = []
+    File.open("matrix.txt") do |f|
+      f.each_line{ |line| matrix << line.split(',').map(&:to_i) }
+    end
+    answer = []
+    matrix.each_with_index do |row, i|
+      answer[i] = []
+      if i == 0
+        row.each_with_index do |elm, j|
+          if j == 0
+            answer[i][j] = matrix[i][j]
+          else
+            answer[i][j] = answer[i][j-1] + matrix[i][j]
+          end
+        end
       else
-        answer[i][j] = answer[i][j-1] + matrix[i][j]
+        row.each_with_index do |elm, j|
+          if j == 0
+            answer[i][j] = answer[i-1][j] + matrix[i][j]
+          else
+            answer[i][j] = [answer[i][j-1], answer[i-1][j]].min + matrix[i][j]
+          end
+        end
       end
     end
-  else
-    row.each_with_index do |elm, j|
-      if j == 0
-        answer[i][j] = answer[i-1][j] + matrix[i][j]
-      else
-        answer[i][j] = [answer[i][j-1], answer[i-1][j]].min + matrix[i][j]
-      end
-    end
+    answer[-1][-1]
   end
 end
-p answer[-1][-1]
+p Problem081.new().compute()
