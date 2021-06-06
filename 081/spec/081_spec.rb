d777e9ea4ce0555a80b9d9316b88dea5cfbd0e51
bornite <tmkoikee@gmail.com>
2021-06-06 21:52:57 +0900

    :bug: fix syntax error

--- 081/spec/081_spec.rb
+++ 081/spec/081_spec.rb
@@ -3,7 +3,7 @@ require "./081"
 RSpec.describe Problem081 do
   describe '#compute' do
     it 'is solve Problem 081' do
-      expect(Problem081.new().compute()).to eq(427337
+      expect(Problem081.new().compute()).to eq(427337)
     end
   end
 end
