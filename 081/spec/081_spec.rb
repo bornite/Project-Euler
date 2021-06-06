1fac218910a28de6d5ede8894308ba9f1fa66949
bornite <tmkoikee@gmail.com>
2021-06-06 21:51:19 +0900

    :rotating_light: Add rspec test code for problem 081

---
+++ 081/spec/081_spec.rb
@@ -0,0 +1,9 @@
+require "./081"
+
+RSpec.describe Problem081 do
+  describe '#compute' do
+    it 'is solve Problem 081' do
+      expect(Problem081.new().compute()).to eq(427337
+    end
+  end
+end
