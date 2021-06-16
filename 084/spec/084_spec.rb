6ab9a4270d9b339fe7af20c0cca771ae3ff3cc54
bornite <tmkoikee@gmail.com>
2021-06-16 19:48:24 +0900

    :fire: clean up.

--- 084/spec/084_spec.rb
+++
@@ -1,9 +0,0 @@
-require "./083"
-
-RSpec.describe Problem083 do
-  describe '#compute' do
-    it 'is solve Problem 083' do
-      expect(Problem083.new().compute()).to eq(425185)
-    end
-  end
-end
