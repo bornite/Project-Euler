38573dfc3ecc6b28aaf2bb2d778687424a116907
bornite <tmkoikee@gmail.com>
2021-06-17 17:59:21 +0900

    :rotating_light: Add rspec test code for problem 021.

--- 021/spec/021_spec.rb
+++ 021/spec/021_spec.rb
@@ -1,6 +1,11 @@
 require "./021"
 
 RSpec.describe Problem021 do
+    describe '#is_amicable' do
+        it 'determine if the sum of a divisor is a amicable number' do
+            expect(Problem021.new().is_amicable(220)).to eq(true)
+        end
+    end
     describe '#compute' do
         it 'is solve Problem 021' do
             expect(Problem021.new().compute()).to eq(31626)
