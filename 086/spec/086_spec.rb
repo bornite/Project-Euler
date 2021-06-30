1d35996c42a2bc308994a44ba5521037f9a17e88
bornite <tmkoikee@gmail.com>
2021-06-30 14:25:25 +0900

    :rotating_light: fix 086_spec.rb

--- 086/spec/086_spec.rb
+++ 086/spec/086_spec.rb
@@ -3,7 +3,7 @@ require "./086"
 RSpec.describe Problem086 do
   describe '#compute' do
     it 'is solve Problem 086' do
-      expect(Problem086.new().compute()).to eq(2772)
+      expect(Problem086.new().compute()).to eq(1818)
     end
   end
 end
