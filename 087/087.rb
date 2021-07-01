2253fd080d47b81d9ac0be55b6423b5af524ab0e
bornite <tmkoikee@gmail.com>
2021-07-01 18:42:51 +0900

    :sparkles: Add Ruby code to solve problem 087.

---
+++ 087/087.rb
@@ -0,0 +1,19 @@
+require 'prime'
+
+max = 50000000
+primes = [
+  Prime.each((max**(1/2.0)).floor.to_i).to_a,
+  Prime.each((max**(1/3.0)).floor.to_i).to_a,
+  Prime.each((max**(1/4.0)).floor.to_i).to_a
+]
+answers = []
+primes[0].each do |square|
+  primes[1].each do |cubic|
+    primes[2].each do |biquadratic|
+      val = square**2 + cubic**3 + biquadratic**4
+      answers << val if val < max
+    end
+  end
+end
+
+p answers.uniq.length
