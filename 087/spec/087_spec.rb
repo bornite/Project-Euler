76a74ce5364c0eed9af10ae989ce22c5def2622d
bornite <tmkoikee@gmail.com>
2021-07-02 11:40:32 +0900

    :bug: Fix typo.

--- 087/spec/087_spec.rb
+++ 087/spec/087_spec.rb
@@ -3,7 +3,7 @@ require "./087"
 RSpec.describe Problem087 do
   describe '#compute' do
     it 'is solve Problem 087' do
-      expect(Problem0867.new().compute()).to eq(1097343)
+      expect(Problem087.new().compute()).to eq(1097343)
     end
   end
 end
