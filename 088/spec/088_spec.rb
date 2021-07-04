8ed42e4695bf6c830ed09152644a1ec9f6303cc2
bornite <tmkoikee@gmail.com>
2021-07-04 10:31:09 +0900

    :rotating_light: Add test.

--- 088/spec/088_spec.rb
+++ 088/spec/088_spec.rb
@@ -1,6 +1,11 @@
 require "./088"
 
 RSpec.describe Problem088 do
+  describe '#divisors' do
+    it 'find the array of divisors of n' do
+      expect(Problem088.new().divisors(40)).to eq([2, 4, 5, 8, 10, 20, 40])
+    end
+  end
   describe '#compute' do
     it 'is solve Problem 088' do
       expect(Problem088.new().compute()).to eq(7587457)
