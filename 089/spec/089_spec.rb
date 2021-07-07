d66f472a6170f95e4b65f41743d8c67685245bb9
bornite <tmkoikee@gmail.com>
2021-07-07 21:18:43 +0900

    :rotating_light: fix test.

--- 089/spec/089_spec.rb
+++ 089/spec/089_spec.rb
@@ -3,7 +3,7 @@ require "./089"
 RSpec.describe Problem089 do
   describe '#compute' do
     it 'is solve Problem 089' do
-      expect(Problem089.new().compute()).to eq(7587457)
+      expect(Problem089.new().compute()).to eq(743)
     end
   end
 end
