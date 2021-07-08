3fb47aad5b20ef15a6b465554a3e4bb5205443a4
bornite <tmkoikee@gmail.com>
2021-07-08 16:19:42 +0900

    :rotating_light: Add test.

--- 089/spec/089_spec.rb
+++ 089/spec/089_spec.rb
@@ -1,6 +1,11 @@
 require "./089"
 
 RSpec.describe Problem089 do
+  describe '#change_roman_to_val' do
+    it 'convert Roman numerals to values' do
+      expect(Problem089.new().change_roman_to_val("DXVI")).to eq(516)
+    end
+  end
   describe '#compute' do
     it 'is solve Problem 089' do
       expect(Problem089.new().compute()).to eq(743)
