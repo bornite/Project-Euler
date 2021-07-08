0fac52d8b5fad31cde97fbe46e63db06322c0f7f
bornite <tmkoikee@gmail.com>
2021-07-09 07:45:17 +0900

    :hammer: Refactor Ruby code for Problem 090.

--- 090/090.rb
+++ 090/090.rb
@@ -1,21 +1,25 @@
-patterns = [*0..9].combination(6).to_a.map do |a|
-  a << 9 if a.include?(6) && !a.include?(9)
-  a << 6 if a.include?(9) && !a.include?(6)
-  a.uniq
-end.repeated_combination(2).to_a.uniq
-
-squares = [*1..9].map{ |i| i**2 }
-count = 0
-patterns.each do |combi|
-  check = squares.map do |sq|
-    if sq < 10
-      (combi[0].include?(sq) && combi[1].include?(0)) ||
-        (combi[0].include?(0) && combi[1].include?(sq))
-    else
-      (combi[0].include?(sq/10) && combi[1].include?(sq%10)) ||
-        (combi[0].include?(sq%10) && combi[1].include?(sq/10))
+class Problem090
+  def compute()
+    patterns = [*0..9].combination(6).to_a.map do |a|
+      a << 9 if a.include?(6) && !a.include?(9)
+      a << 6 if a.include?(9) && !a.include?(6)
+      a.uniq
+    end.repeated_combination(2).to_a.uniq
+    squares = [*1..9].map{ |i| i**2 }
+    count = 0
+    patterns.each do |combi|
+      check = squares.map do |sq|
+        if sq < 10
+          (combi[0].include?(sq) && combi[1].include?(0)) ||
+            (combi[0].include?(0) && combi[1].include?(sq))
+        else
+          (combi[0].include?(sq/10) && combi[1].include?(sq%10)) ||
+            (combi[0].include?(sq%10) && combi[1].include?(sq/10))
+        end
+      end.inject(:&)
+      count += 1 if check
     end
-  end.inject(:&)
-  count += 1 if check
+    count
+  end
 end
-p count
+p Problem090.new().compute()
