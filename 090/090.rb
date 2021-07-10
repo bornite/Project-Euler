9f4d218175dd4f0f4d3a2e14c4537479960ecf5b
bornite <tmkoikee@gmail.com>
2021-07-10 16:57:07 +0900

    :hammer: Refactor Ruby code for Problem 090.

--- 090/090.rb
+++ 090/090.rb
@@ -1,11 +1,14 @@
 class Problem090
+  def get_squares(a)
+    squares = a.map{ |i| i**2 }
+  end
   def compute()
     patterns = [*0..9].combination(6).to_a.map do |a|
       a << 9 if a.include?(6) && !a.include?(9)
       a << 6 if a.include?(9) && !a.include?(6)
       a.uniq
     end.repeated_combination(2).to_a.uniq
-    squares = [*1..9].map{ |i| i**2 }
+    squares = get_squares([*1..9])
     count = 0
     patterns.each do |combi|
       check = squares.map do |sq|
